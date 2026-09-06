7ae6c00fedf172469c70ef54bfc31060d3a6a9a7
QC3284 <QC3284@users.noreply.github.com>
2026-09-06 15:46:06 +0800

    runner-side python trace fix

---
+++ fix_trace.py
@@ -0,0 +1,49 @@
+import sys
+p = 'kernel/trace/trace.c'
+s = open(p).read()
+broken = '''\tmutex_lock(&event_mutex);
+
+\t/* Fail if the file is marked for removal */
+\tif (file->flags & EVENT_FILE_FL_FREED) {
+\t\ttrace_array_put(file->tr);
+\t\tret = -ENODEV;
+\t} else {
+\t\tevent_file_get(file);
+\t}
+
+\tmutex_unlock(&event_mutex);
+\tif (ret)
+\t\treturn ret;
+
+\tfilp->private_data = inode->i_private;'''
+fixed = '''\tfilp->private_data = inode->i_private;'''
+assert broken in s, 'generic_tr block not found'
+s = s.replace(broken, fixed, 1)
+anchor = '''\tret = tracing_check_open_get_tr(file->tr);
+\tif (ret)
+\t\treturn ret;
+
+\tfilp->private_data = inode->i_private;'''
+repl = '''\tret = tracing_check_open_get_tr(file->tr);
+\tif (ret)
+\t\treturn ret;
+
+\tmutex_lock(&event_mutex);
+
+\t/* Fail if the file is marked for removal */
+\tif (file->flags & EVENT_FILE_FL_FREED) {
+\t\ttrace_array_put(file->tr);
+\t\tret = -ENODEV;
+\t} else {
+\t\tevent_file_get(file);
+\t}
+
+\tmutex_unlock(&event_mutex);
+\tif (ret)
+\t\treturn ret;
+
+\tfilp->private_data = inode->i_private;'''
+assert anchor in s, 'file_tr anchor not found'
+s = s.replace(anchor, repl, 1)
+open(p, 'w').write(s)
+print('trace.c fixed' if 'EVENT_FILE_FL_FREED' in s else 'FAILED')
